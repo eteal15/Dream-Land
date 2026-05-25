124dc0fd3e2184149afac86f7a923a1473702853
eteal15 <etealminas@gmail.com>
2026-05-25 14:33:55 +0300

    chore: use environment variable for server port

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ import { ReligionType, PaymentPlanType } from "./src/lib/types";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // Set up server-side parse body limits
 app.use(express.json({ limit: "50mb" }));
